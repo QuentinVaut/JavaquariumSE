5bf2c3f1e82acbe3c172d017f1926829e2f515a2
QuentinVaut <quentin-vauthier@hotmail.fr>
2017-02-01 09:26:16 +0100

    Abstract class with interface

--- src/Poissons/PoissonCarnivore.java
+++ src/Poissons/PoissonCarnivore.java
@@ -3,7 +3,7 @@ package Poissons;
 /**
  * Created by quentin on 31/01/2017.
  */
-public abstract class PoissonCarnivore extends Poisson {
+public abstract class PoissonCarnivore extends Poisson implements Carnivore {
 
     public PoissonCarnivore(String nom, Genre genre) {
         super(nom, genre);
--- src/Poissons/PoissonHerbivore.java
+++ src/Poissons/PoissonHerbivore.java
@@ -3,11 +3,12 @@ package Poissons;
 /**
  * Created by quentin on 31/01/2017.
  */
-public abstract class PoissonHerbivore extends Poisson {
+public abstract class PoissonHerbivore extends Poisson implements Herbivore {
     public PoissonHerbivore(String nom, Genre genre) {
         super(nom, genre);
     }
 
+    @Override
     public void mange(Algue algue) {
 
     }
