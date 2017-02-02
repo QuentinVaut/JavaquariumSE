be1534c3140958e9493efa3ba0992986ae691638
QuentinVaut <quentin-vauthier@hotmail.fr>
2017-02-02 11:57:57 +0100

    Correction PoisonHerbivore

--- src/Poissons/PoissonHerbivore.java
+++ src/Poissons/PoissonHerbivore.java
@@ -14,7 +14,7 @@ public abstract class PoissonHerbivore extends Poisson implements Herbivore {
     public void mange(Algue algue) {
         System.out.println("Poisson : " + this.getNom() + " mange : algue");
         algue.setPv(algue.getPv() - 2);
-        if(algue.getPv() == 0) {
+        if(algue.getPv() <= 0) {
             algue.die();
         }
     }
